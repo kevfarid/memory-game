cf006e5a79e65d84dfc76bc050ddd8142554619b
Kevin Cruz <kevfaridc@gmail.com>
2023-04-27 17:15:13 -0500

    fix: volume sounds

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   } = useGame();
 
   const [playMusic] = useSound('/sounds/Wholesome.mp3', {
-    volume: isMuted ? 0 : 0.3,
+    volume: isMuted ? 0 : 0.1,
     interrupt: false,
   });
 
